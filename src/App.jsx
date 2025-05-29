113f6b3183a13e608426c6c85962f82ca051a53f
Rizwan <mdrizwan6386@gmail.com>
2025-05-29 21:55:47 +0530

    added blog2 and meta data for 2nd blog and image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Blog1 from './components/Blog1';
 import FrontPage from './components/Frontpage';
+import Blog2 from './components/blog2';
 
   function App() {
   return (
@@ -9,6 +10,7 @@ import FrontPage from './components/Frontpage';
       <Routes>
         <Route path="/" element={<FrontPage />} />
         <Route path="/blog1" element={<Blog1 />} />
+         <Route path="/blog2" element={<Blog2 />} />
       </Routes>
     </Router>
    
